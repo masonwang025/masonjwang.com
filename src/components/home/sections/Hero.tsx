b52a94a88ee6bcb87d80fe4da538127cbe9183ec
masonwang025 <masonwang025@gmail.com>
2024-02-29 11:12:16 -0500

    checkpoint

--- src/components/home/sections/Hero.tsx
+++ src/components/home/sections/Hero.tsx
@@ -1,10 +1,10 @@
 function Hero() {
-  return <div className="pt-28 pb-14 container mx-auto min-h-screen flex flex-col">
+  return <div className="pt-28 pb-14 container mx-auto min-h-screen">
     <div>
       <div><p className="header text-xl text-gray-300 text-center">What is the meaning of life?</p></div>
-      <div><p className="header text-xl text-gray-500 pt-4 pb-3">My best friend told me,</p></div>
     </div>
     <div className="mx-auto container max-w-xl">
+      <div><p className="header text-xl text-gray-500 pt-4 pb-3">My best friend told me,</p></div>
       <div className="flex flex-1 grow flex-col space-y-4 pb-10">
         <div>
           <p className="header text-xl text-left">&ldquo;You are born,</p>
