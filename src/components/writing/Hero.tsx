8e7d246ae54caf7b70325b997d65e9be39e4d12e
masonwang025 <masonwang025@gmail.com>
2024-06-11 18:46:37 -0700

    mmm

--- src/components/writing/Hero.tsx
+++ src/components/writing/Hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Hero({ title, publishedAt, image }: { title: string, publishedAt: string, image?: string }) {
     return (
         <div className="bg-background-dark text-white flex flex-col justify-center items-center">
-            <div className="vertical center-h space-between h-screen py-14 md:py-24">
+            <div className="vertical center-h space-between h-[calc(100dvh)] py-14 md:py-24">
                 <Link className="text-sm text-neutral-400" href="/writing">← back to writing</Link>
                 <div>
                     <h1 className="text-balance title font-semibold text-2xl">
