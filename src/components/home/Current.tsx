6fd25bf2de83f793e5cd090913f9958804c4c7de
masonwang025 <masonwang025@gmail.com>
2024-06-19 01:39:30 -0700

    fix

--- src/components/home/Current.tsx
+++ src/components/home/Current.tsx
@@ -3,7 +3,7 @@ function Current() {
         <p><b>current focus</b></p>
         <p><i>AI alignment</i></p>
         <ol className="list-decimal ml-4 space-y-1.5">
-            <li>i think this is important problem to work on. for now this looks like a) exploring mech interp & W2SG research directions, b) talking to people in the space, and c) figuring out the rest.</li>
+            <li>i think this is the most important problem to work on. for now this looks like a) exploring mech interp & W2SG research directions, b) talking to people in the space, and c) figuring out the rest.</li>
             <li>i'm building <b><a href="https://alignment.guide" target="_blank" className="link">alignment.guide</a></b> to help others navigate the space.</li>
             <li>just submitted <i>Model Mapping: Decompiling Transformers into Python Programs</i> to ICML.</li>
         </ol>
