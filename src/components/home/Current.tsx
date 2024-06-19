30bb9abc6709a7eb1c3a2e291bf40849280af475
Mason <masonwang025@gmail.com>
2024-06-18 23:15:57 -0400

    Update Current.tsx

--- src/components/home/Current.tsx
+++ src/components/home/Current.tsx
@@ -4,7 +4,7 @@ function Current() {
         <p><i>AI alignment</i></p>
         <ol className="list-decimal ml-4 space-y-1.5">
             <li>i honestly don't know what this concretely means yet, but for now it's a) exploring mech interp & W2SG research directions, b) talking to people in the space, and c) figuring out the rest.</li>
-            <li>i built <b><a href="https://alignment.guide" target="_blank" className="link">alignment.guide</a></b> to help others navigate the space.</li>
+            <li>i'm building <b><a href="https://alignment.guide" target="_blank" className="link">alignment.guide</a></b> to help others navigate the space.</li>
             <li>just submitted <i>Model Mapping: Decompiling Transformers into Python Programs</i> to ICML.</li>
         </ol>
         <p>(p.s. i'm not a doomer i promise)</p>
