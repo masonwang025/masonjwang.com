e9eff2a0dacf5f52be8a650dfba05c6f245a07cd
masonwang025 <masonwang025@gmail.com>
2024-06-19 01:26:51 -0700

    done

--- src/components/home/Previous.tsx
+++ src/components/home/Previous.tsx
@@ -1,7 +1,7 @@
 function Previous() {
     return (<div className="vertical space-y-2">
         <p><b>previously</b></p>
-        <ul className="italic space-y-1.5">
+        <ul className="italic space-y-4">
             {/* <li>incoming student @ Stanford</li> */}
             <li>
                 NLP research @{" "}
@@ -12,13 +12,23 @@ function Previous() {
                 >
                     Stanford's Open Virtual Assistant Lab
                 </a>
+                <ul className="not-italic ml-4 list-disc space-y-1.5 mt-1.5">
+                    <li>built Noora, a platform to help people with autism improve social conversation</li>
+                    <li>first high-schooler to present to <a className="link" href="https://nlp.stanford.edu/" target="_blank" rel="noopener noreferrer">Stanford NLP Group</a></li>
+                    <li>guest speaker at CS 372: AI for Precision Medicine & Psychiatric Disorders</li>
+                </ul>
             </li>
+
             <li>
-                ex-founder @ Hazel (backed by{" "}
-                <a target="_blank" href="https://pear.vc/" className="link">
-                    Pear VC
-                </a>{" "}
-                and others)
+                cofounder @ Hazel
+                <ul className="not-italic ml-4 list-disc space-y-1.5 mt-1.5">
+                    <li>deferred freshman year to build Siri for realtors</li>
+                    <li>raised seed round at 17 (backed by{" "}
+                        <a target="_blank" href="https://pear.vc/" className="link">
+                            Pear VC
+                        </a>{" "}
+                        and others)</li>
+                </ul>
             </li>
         </ul>
     </div>);
