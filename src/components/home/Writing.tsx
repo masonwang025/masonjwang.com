07c8fbc8fff25ba8c43114664d95c19720f26855
masonwang025 <masonwang025@gmail.com>
2024-07-24 00:29:06 -0700

    update on alignment.guide

--- src/components/home/Writing.tsx
+++ src/components/home/Writing.tsx
@@ -4,6 +4,8 @@ function Writing() {
     return (<div className="my-8 vertical space-y-3">
         <p><b>writing</b></p>
         <BlogPosts />
+        <p>also, for what it's worth: i was previously writing <a href="https://alignment.guide" target="_blank" className="link">alignment.guide</a> but have since tabled that project.</p>
+
     </div>);
 }
 
