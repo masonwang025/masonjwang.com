d8191b3688ac781e85872d7e8af55f34f64e89e3
Mason <masonwang025@gmail.com>
2024-11-12 03:34:14 -0800

    Update Current.tsx

--- src/components/home/Current.tsx
+++ src/components/home/Current.tsx
@@ -2,9 +2,6 @@ function Current() {
     return (<div className="vertical space-y-2">
         <p><b>currently</b></p>
         <p><i>~ Tilde (<a href="https://tilderesearch.com" target="_blank" className="link">tilderesearch.com</a>)</i></p>
-        <p>
-            coming soon.
-        </p>
     </div>);
 }
 
